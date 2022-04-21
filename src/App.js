524ddc37db0d5c4219e11762ddc758acdd646718
gnuh96 <ntuanhung.96@gmail.com>
2022-04-22 00:31:46 +0200

    Lab: TodoList

--- src/App.js
+++ src/App.js
@@ -1,64 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import React from 'react';
+import TodoFeature from './features/Todo';
 
 function App() {
-  const name = 'Tuan Hung NGUYEN';
-  const age = 25;
-  const isMale = true;
-  const student = {
-    name : 'Easy Frontend'
-  };
-  const colorList = ['red', 'green', 'blue'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ReactJs
-        </p>
-        <p>Bonjour {name} - {age} - {isMale ? 'Male' : 'Female'}</p>
-        
-        {/* Solution 1 */}
-        {isMale ? <p>Male</p> : <p>Female</p>}
-       
-        {/* Solution 2 */}
-        {isMale && <p>Male</p>}
-        {!isMale && <p>Female</p>}
-
-        {/* Render 3 lignes dans un props */}
-        {isMale && (
-          <div>
-            <p>Male 1</p>   
-            <p>Male 2</p>   
-            <p>Male 3</p>
-          </div>   
-        )}
-        {isMale && (
-          <React.Fragment>
-            <p>Male 1</p>   
-            <p>Male 2</p>   
-            <p>Male 3</p>
-          </React.Fragment>   
-        )}
-        {isMale && (
-          <>
-            <p>Male 1</p>   
-            <p>Male 2</p>   
-            <p>Male 3</p>
-          </>   
-        )}
-
-        {/* Render object */}
-        <p>{student.name}</p>
-
-        {/* Render list */}
-        <ul>
-          {colorList.map(color => (
-            <li style={{color}}>{color}</li>
-          ))}
-        </ul>
-      </header>
+      <TodoFeature />
     </div>
   );
 }
