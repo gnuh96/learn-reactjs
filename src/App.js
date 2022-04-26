d79828515caa1e2713f0e096e215aa5c242ec5da
gnuh96 <ntuanhung.96@gmail.com>
2022-04-27 00:19:31 +0200

    Active Todo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 // import './App.css';
 import React from 'react';
-import ColorBox from './components/ColorBox';
-import Counter from './components/Counter';
-import AlbumFeature from './features/Album';
+import TodoFeature from './features/Todo';
 
 function App() {
   return (
     <div className="App">
-      {/* <TodoFeature /> */}
+      <TodoFeature />
       {/* <AlbumFeature /> */}
-      <ColorBox />
-      <Counter />
+      {/* <ColorBox /> */}
+      {/* <Counter /> */}
     </div>
   );
 }
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import TodoList from './components/TodoList';
 
@@ -7,24 +7,44 @@ TodoFeature.propTypes = {
 };
 
 function TodoFeature(props) {
-    const todoList = [
+    const initTodoList = [
         {
             id: 1,
-            title: 'Eat'
+            title: 'Eat',
+            status: 'new'
         },
         {
             id: 2,
-            title: 'Sleep'
+            title: 'Sleep',
+            status: 'completed'
         },
         {
             id: 3,
-            title: 'Code'
+            title: 'Code',
+            status: 'new'
         }
-    ]
+    ];
+
+    const [todoList, setTodoList] = useState(initTodoList);
+
+    const handleTodoClick = (todo, index) => {
+        console.log(todo, index);
+        // clone current array
+        const newTodoList = [...todoList];
+        // toggle state
+        const newTodo = {
+            ...newTodoList[index],
+            status: newTodoList[index].status === 'new' ? 'completed' : 'new',
+        }
+        newTodoList[index] = newTodo;
+        // update todolist
+        setTodoList(newTodoList);
+    }
+
     return (
         <div>
             <h3>Todo List</h3>
-            <TodoList todoList={todoList} />
+            <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
         </div>
     );
 }
